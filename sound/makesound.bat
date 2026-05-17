cd2adc1aa57d0f70271151c51a1df9b045fcdf69
DaxKatter2117 <daxternator2117@gmail.com>
2026-05-18 05:28:59 +0800

    Fix bug with tabs/spaces being included in macro label output

--- sound/makesound.bat
+++ sound/makesound.bat
@@ -1,5 +1,5 @@
 @echo off
-set VASMZ80="_tools/VASM/vasmz80_psi-x.exe" -altlocal -altnum -spaces -maxerrors=0 -Fbin -start=0
+set VASMZ80="_tools/VASM/vasmz80_psi-x.exe" -altlocal -altnum -spaces -maxerrors=0 -Fbin -start=0 -opt ws+
 
 %VASMZ80% -o "_out/sound.z80" -L "_out/sound.lst" -Lall "sound.asm" 2> _out/log.txt
 
