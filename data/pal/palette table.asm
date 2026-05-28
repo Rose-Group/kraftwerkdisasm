5fae86bc5f50d31280de6c2f4790470bad1df522
DaxKatter2117 <daxternator2117@gmail.com>
2026-05-28 21:50:04 +0800

    More data splitting
    Robotnik's Art Data and Palette Data

---
+++ data/pal/palette table.asm
@@ -0,0 +1,176 @@
+Pal_Black:
+	incbin	"data/pal/fix/00 - Black.pal"
+Pal_RedYellowPuyos:
+	incbin	"data/pal/fix/01 - Red Yellow Puyos.pal"
+Pal_BluePurplePuyos:
+	incbin	"data/pal/fix/02 - Blue Purple Puyos.pal"
+Pal_GreenTealPuyos:
+	incbin	"data/pal/fix/03 - Green Teal Puyos.pal"
+Pal_General_Puyo:
+	incbin	"data/pal/fix/04 - General (Puyo).pal"
+Pal_GrassIntroBG_Puyo:
+	incbin	"data/pal/fix/05 - Cutscene Grass Sky (Puyo).pal"
+Pal_GrassIntroFG_Puyo:
+	incbin	"data/pal/fix/06 - Cutscene Grass FG (Puyo).pal"
+Pal_RuinsIntroFG_Puyo:
+	incbin	"data/pal/fix/07 - Cutscene Ruins FG (Puyo).pal"
+Pal_RuinsIntroBG_Puyo:
+	dc.w 0, $A46, $C46, $E46, $E66, $E86, $E84, $C82, $EA8, $C86, $A64, $A42, $820, $600, 0, $CCE
+Pal_SatanIntro_Puyo:
+	dc.w 0, $400, $400, $600, $800, $A00, $C00, $E00, $42E, $202, $204, $206, $208, $20C, $200, $EEE
+Pal_GameOver_FG:
+	dc.w 0, $422, $644, $866, $A88, $2A4, $2C6, $2C8, $2E8, $2EA, $6EC, $AEC, $6EE, $200, $FFF, $FFF
+Pal_GameOver_BG:
+	dc.w 0, 0, $200, 0, $600, $600, 0, $802, $804, $806, $808, $A0A, $FFF, $FFF, $FFF, $FFF
+Pal_White:
+	dc.w $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE
+Pal_White2:
+	dc.w $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE, $EEE
+Pal_CabinBoard_Puyo:
+	dc.w 0, 0, $46A, $A, 8, 4, $ACA, $EEE, $4A, $8A, 6, 2, $2A, $22, 0, $60
+Pal_EndingBG_Puyo:
+	dc.w 0, 0, $604, $406, 8, $A, $2C, $4E, $6E, $8E, $AE, $CE, $6EE, $E0E, $E0E, $EEE
+Pal_TitleArle_Puyo:
+	dc.w 0, 0, $8AE, $2E, $C, $228, $EE6, $A66, $48E, $CE, $846, $EA0, $6E, $466, $8AA, $EEE
+Pal_StaffBG_Puyo:
+	dc.w 0, 0, 0, $400, $600, $820, $A42, 0, $C44, $E66, $E88, $EAA, $ECC, $800, $A64, $EEE
+Pal_HighScoresBG:
+	dc.w 0, $888, $CEE, $68, $AC, $A8A, $666, $AAA, $868, $AAC, $444, $668, $466, $222, $A88, $ACC
+Pal_EndingArleNormal_Puyo:
+	dc.w 0, 0, $8AE, $2E, $C, $228, $EE6, $A66, $48E, $CE, $846, $EA0, $6E, $466, $8AA, $EEE
+Pal_EndingArleFade_Puyo:
+	dc.w 0, 0, $46A, $A, 8, 4, $A82, $622, $4A, $8A, $440, $A2, $2A, $22, $686, $AAA
+Pal_Options:
+	dc.w 0, 0, $60, $80, $A4, $E4, $AEA, $400, $664, $684, $8A4, $CE4, $6C8, $200, $620, $EEE
+Pal_Unknown1_Puyo:
+	dc.w $EEE, $EEE, $888, $888, $AAA, $AAA, $444, $888, $444, $222, $AAA, $444, $666, $888, $444, 0
+Pal_MainMenu_Puyo:
+	dc.w 0, 0, $8AE, $2E, $C, $228, $EE6, $A66, $48E, $CE, $846, $EA0, $6E, $466, $8AA, $EEE
+Pal_PractiseMode_Puyo:
+	dc.w 0, 0, $8AE, $2E, $C, $228, $ACA, $EEE, $4AE, $CE, $400, $200, $6E, $466, $200, $620
+Pal_OpponentPreview_Puyo:
+	dc.w 0, 0, $446, $E0E, $24, $224, $E0E, $E0E, $46, $CE, $E0E, $E0E, $46, $E0E, $A64, $68
+Pal_OptionText_Gray:
+	dc.w 0, $222, $444, $666, $888, $AAA, $CCC, $EEE, 0, 0, 0, 0, 0, 0, $4EA, $EEE
+Pal_OptionText_Blue:
+	dc.w 0, $200, $400, $600, $800, $A00, $C00, $E00, 0, 0, 0, 0, 0, 0, $4EA, $C22
+Pal_OptionText_Red:
+	dc.w 0, 2, 4, 6, 8, $A, $C, $E, 0, 0, 0, 0, 0, 0, $4EA, $22C
+Pal_OptionText_Green:
+	dc.w 0, $20, $40, $60, $80, $A0, $C0, $E0, 0, 0, 0, 0, 0, 0, $4EA, $2C2
+Pal_TutorialJoystick:
+	dc.w 0, 0, $8EE, $AE, $4A, $28, 6, $EEE, $6C, $CE, 0, $200, $666, $AAA, 0, 0
+Pal_TitleBG_Puyo:
+	dc.w 0, $422, $244, $246, $266, $444, $268, $668, $686, $688, $68A, $6AA, $6AC, $220, $ACE, $CCC
+Pal_Unknown2_Puyo:
+	dc.w 0, 0, $ACE, $20E, $26C, $24A, $EC0, $EA2, $8AE, $2AE, $A42, $E64, $48C, $886, $CCA, $CEE
+Pal_TitleBlue_Puyo:
+	dc.w 0, 0, $600, $800, $A00, $C00, $E20, $E40, $E40, $E60, $E82, $EA4, $EC6, $EEA, $CCA, $EEE
+Pal_TitleGreen_Puyo:
+	dc.w 0, 0, $40, $60, $80, $A0, $A0, $C0, $C0, $2C0, $4C0, $6E0, $8E4, $CEA, $CCA, $EEE
+Pal_TitleRed_Puyo:
+	dc.w 0, 0, 6, 8, $A, $C, $C, $E, $20E, $22E, $44E, $66E, $8AE, $CCE, $CCA, $EEE
+Pal_TitleYellow_Puyo:
+	dc.w 0, 0, $48, $48, $6C, $8A, $AE, $CE, $CE, $2CE, $4EE, $6EE, $6EE, $8EE, $CCC, $EEE
+Pal_TitleCyan_Puyo:
+	dc.w 0, 0, $862, $862, $A80, $CA0, $EC0, $EC0, $EE0, $EE2, $EE6, $EE8, $EEC, $EEC, $CCA, $EEE
+Pal_TitlePurple_Puyo:
+	dc.w 0, 0, $606, $606, $808, $A0A, $E0C, $E2E, $E4E, $E6E, $E8E, $EAE, $ECE, $EEE, $CCA, $EEE
+Pal_RuinsStage_Puyo:
+	dc.w 0, 0, $60, $80, $A4, $E4, $AEA, $440, $666, $688, $8AA, $ACC, $CEE, $400, $444, $EEE
+Pal_SatanStage_Puyo:
+	dc.w 0, 0, $60, $80, $A4, $E4, $AEA, $440, $446, $668, $88A, $AAC, $CCE, $400, $224, $EEE
+
+	dc.w $464, 0, $6AE, $48C, $246, $64E, $42C, $20A, 6, $ECC, $EAA, $C68, $644, $4CE, $8E, $EEE
+
+	dc.w $600, 0, $4CE, $AE, $8C, $6A, $2C, $24E, $46, $24, $422, $666, $A88, $CAA, $ECC, $EEE
+
+	dc.w $600, 0, $EAC, $E8A, $C68, $A66, $A46, $CCA, $622, $402, 4, $888, $AC, $AEE, $24C, $EEE
+
+	dc.w $600, 0, $26, $24A, $68C, $8AE, $ACE, $866, $ECA, $A88, $2AE, $46C, $8CE, $422, 4, $EEE
+
+	dc.w $600, 0, 8, 6, 4, 2, $E, $2E, $6E, $CCC, $2AE, $222, $444, $666, $AAA, $EEE
+
+	dc.w $600, 0, $420, $642, $A82, $CC4, $2AC, $46, $440, $CCA, $6A, $222, $444, $888, $C, $EEE
+
+	dc.w $600, 0, $CAE, $86E, $64C, $428, $4CE, $602, $824, $C46, $E8A, $26A, $888, $EAC, $E68, $EEE
+
+	dc.w $600, 0, $26, $6C, $2AE, $6CE, $4A, $CAA, $24, 4, $866, $22, $244, $466, $688, $EEE
+
+	dc.w 0, 0, $26, $24A, $68C, $8AE, $ACE, $644, $CAA, $A66, $6C, $46A, $28E, $422, 2, $EEE
+
+	dc.w 0, $EEE, $884, $862, $ACA, $6A8, $486, $264, $6AE, $62, $A4, $4C8, $26, $4A, $28C, $EEE
+
+	dc.w 0, 0, $222, $666, $AAA, $8CE, $48A, $6AC, $EEE, $E84, $EA4, $EAA, $E84, $EC6, $EEA, $EEE
+Pal_RobotnikLair:
+	dc.w $222, $A66, $E88, $EAA, $ECC, $844, 0, $222, $444, $888, $CCC, $4AA, $666, $24, $6C, $EEE
+Pal_OpeningGrounder:
+	dc.w 0, 0, $420, $642, $A82, $CC4, $2AC, $66, $C, $26A, $6AC, $222, $444, $888, $CCA, $EEE
+Pal_CutsceneRobotnik:
+	dc.w 0, 0, $6AE, $48C, $246, $64E, $42C, $20A, 6, $ECC, $EAA, $C68, $644, $4CE, $8E, $EEE
+
+	dc.w $FFFF, 0, $6CE, $AE, $6A, $EAA, $A86, $24E, $26, 6, $222, $444, $642, $888, $CCC, $EEE
+
+	dc.w 0, 0, $20, $240, $260, $280, $2A4, $4C6, $AEA, 6, $4E, $46, $268, $48A, $6AC, $EEE
+
+	dc.w 0, 0, $20, $240, $260, $280, $2A4, $4C6, $AEA, 0, 0, $46, $268, $AAA, $EEE, $EEE
+
+	dc.w 0, 0, 2, 4, $A, $2C, $6E, $2AE, $8CE, $EEE, 0, 0, 0, 0, 0, $EE0
+Pal_MainMenuShadow:
+	dc.w 0, 0, $20, 0, 0, 0, 0, 0, 0, 6, $4E, 0, 0, 0, 0, $24
+
+	dc.w $600, 0, $28E, 4, $42C, $C, $222, $24, $444, $4A, $4CE, $666, $AAA, $CCC, $888, $EEE
+
+	dc.w 0, 0, $EAC, $E8A, $C68, $A66, $A46, $CCA, $622, $2C, 4, 0, $28C, $AEE, $48, $EEE
+
+	dc.w $600, 0, $8A, $40, $82, $2C4, $666, $AAA, $24, $68, $CE, $22, $244, $466, $688, $EEE
+Pal_Robotnik:
+	dc.w $600, 0, $6AE, $46C, $248, $24, $42C, $20A, 4, $4A, $ECC, $C8A, $644, $4CE, $8E, $EEE
+
+	dc.w $600, 0, $E8E, $C6C, $A4C, $82A, $608, $666, $204, $202, $AAA, $22, $244, $466, $688, $EEE
+
+	dc.w 0, 0, $CAE, $86E, $64C, $228, $4CE, $802, $824, $A46, $E6A, $204, $400, $EAC, $E48, $EEE
+
+	dc.w 0, 0, 8, 6, 4, 2, $4E, $8E, $8CE, $222, $444, $666, $888, $AAA, $CCC, $EEE
+
+	dc.w $600, 0, $26, $24A, $46C, $68E, $AAE, $A64, $620, $842, $C86, $EAA, $ECC, $8A, $CC, $EEE
+
+	dc.w $600, 0, $2E, 6, $8E, $48, $CCA, 4, $444, 2, $6A, $888, $6CE, $A, $EE, $EEE
+
+	dc.w 0, 0, 2, 4, 8, $2A, $24, $444, $48, $6C, $4AE, $666, $AAA, $888, $222, $EEE
+
+	dc.w 0, $EEE, 0, $AA8, 6, $2A, $24C, $48E, $666, 6, $2A, $24C, $48E, $666, $EEE, $EEE
+
+	dc.w 0, 0, $46, $8C, $4CE, 0, $E4, 2, $48, $4C, $48E, $26, $68, $AC, $8EE, $EEE
+
+	dc.w 0, 0, 4, $22A, $86E, $C, $222, $24, $8A, $20, $2AE, $666, $AAA, $4A2, $260, $EEE
+
+	dc.w 0, 0, 6, $48, $46C, $8AE, $68, $CE, $420, $640, $A62, $C86, $EC8, $864, $EE, $EEE
+
+	dc.w 0, 0, $46, $88, $CC, $6EE, $40, $62, $A4, $6CA, $408, $222, $444, $666, $AAA, $EEE
+
+	dc.w 0, 0, $420, $642, $A82, $CC4, $2AC, $46, $20C, $268, $48C, $222, $444, $888, $CCA, $EEE
+Pal_VSOhNo:
+	dc.w 0, 0, $EEE, $464, $686, $8A8, $AEA, $400, $602, $804, $C28, $A06, $E2A, $FFFF, $FFFF, $EEE
+
+	dc.w 0, 0, $E8E, $C6C, $A4C, $82A, $608, $406, $202, $CA8, $200, $420, $842, $A64, $C86, $EEE
+
+	dc.w 0, 0, $6CE, $AE, $6A, $EAA, $A86, $24E, $26, 6, $222, $444, $642, $888, $CCC, $EEE
+
+	dc.w $200, 0, $200, $222, $422, $644, $866, $A88, $CAA, $ECC, $48, $26, $2AC, 4, $46C, $EEE
+
+	dc.w 0, 6, $48, 0, $800, $A40, 0, $480, $260, $4E0, 0, $22, $24, $24, $24, $EEE
+
+	dc.w 0, $20, $60, $A0, $E4, $EEE, $802, $A40, $C62, $E84, $EC8, $26, $4E, $AE, $2EE, 0
+
+	dc.w 0, 0, $A, $8E, $4C, 6, $200, $200, $200, $200, $600, $A04, $E08, $C48, $E8A, $EEE
+
+	dc.w 0, $888, $CEE, $68, $AC, $A8A, $666, $AAA, $868, $AAC, $444, $668, $466, $222, $A88, $ACC
+
+	dc.w 0, 0, $80, $A4, $E4, $AEA, $666, $AAA, $868, $AAC, $444, $668, $466, $AAA, $EEE, $EEE
+
+	dc.w 0, 0, $AAA, $48, $4CE, $AC, $6A, $E4E, $E0E, $808, $604, $2E8, $C0, $60, $40, $EEE
+
+	dc.w 0, 0, $EC0, $EA0, $E80, $E60, $E40, $E20, $E00, $C00, $A00, 0, 0, 0, 0, 0
+
+	dc.w 0, 0, $408, $2A, $44C, $400, $800, $A80, $64C, $8E, $AE, $EE, $8EE, $CA4, $EC8, $EEA
