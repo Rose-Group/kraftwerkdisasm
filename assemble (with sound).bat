1056eac3e2b3c492571c29932535f68ee20927ad
DaxKatter2117 <daxternator2117@gmail.com>
2026-05-28 18:24:33 +0800

    Added output folder for assembler output files (excl output ROM)

--- assemble (with sound).bat
+++ assemble (with sound).bat
@@ -7,4 +7,6 @@ cd ..
 tools\asm68k\asm68k /p /o ae- /o l. robotonic.asm, Robotonic.gen, , Robotonic.lst > errors.txt
 type errors.txt
 tools\fixheader Robotonic.gen
+move errors.txt _out\errors.txt
+move Robotonic.lst _out\Robotonic.txt
 pause
